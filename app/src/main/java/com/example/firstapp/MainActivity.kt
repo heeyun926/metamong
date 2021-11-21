09e3f2b74fe416b702f63f28c74dede5f2f96415
SPINNER PRACTICE <fkffkfk31>
2021-11-22 06:14:59 +0900

    Activate button after clicking the radio button - done

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -6,19 +6,21 @@ import android.util.Log
 import com.example.firstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.btnNext1.setEnabled(false)
         binding.radioGroup1.setOnCheckedChangeListener {group, checkId ->
             when(checkId){
-            R.id.pencilSelect -> Log.d("RadioButton","다음 버튼을 클릭해주세요")
-            R.id.talkSelect -> Log.d("RadioButton","다음 버튼을 클릭해주세요")
-            }
+            R.id.pencilSelect -> binding.btnNext1.setEnabled(true)
+            R.id.talkSelect -> binding.btnNext1.setEnabled(true)
 
+            }
         }
+
 
     }
 
