deee2064f2fe94c7bf1252f6b6f9be839723fce1
SPINNER PRACTICE <fkffkfk31>
2022-01-02 02:42:28 +0900

    fragment-activity data connection

--- app/src/main/java/com/example/metamong/fragment/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/metamong/fragment/mypage/MypageFragment.kt
@@ -1,6 +1,7 @@
 package com.example.metamong.fragment.mypage
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,22 +9,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.metamong.R
 import com.example.metamong.databinding.FragmentMypageBinding
 import com.example.metamong.fragment.home.SharemongAdapter
 import com.example.metamong.fragment.home.SharemongData
-import com.example.metamong.fragment.mypage.DB.Memo
-import com.example.metamong.fragment.mypage.DB.MemoDB
+import com.example.metamong.fragment.mypage.DB.*
 
 
+class MypageFragment() : Fragment(){
+     private var binding: FragmentMypageBinding? = null
+     private val viewModel: MemoViewModel by viewModels()
 
- class MypageFragment() : Fragment(){
-    private var binding: FragmentMypageBinding? = null
-    private var memoDB: MemoDB? = null
-    private var memolist = listOf<Memo>()
-
+    private val newMypageFragmentRequestCode = 1
+/**
     companion object {
         fun newInstance(): MypageFragment {
             val fragment = MypageFragment()
@@ -33,30 +35,57 @@ import com.example.metamong.fragment.mypage.DB.MemoDB
 
             return fragment
         }
-    }
+    }**/
 
     override fun onCreateView(
-
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val mBinding = FragmentMypageBinding.inflate(inflater, container, false)
+
+    val mBinding = FragmentMypageBinding.inflate(inflater, container, false)
         binding = mBinding
+
         return binding?.root
     }
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("roomFragment","open")
+        var recyclerView = binding!!.recyclerMongs2
+        var adapter = MongsAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
 
         binding?.btnPlus?.setOnClickListener{
                val intent = Intent(context, MongsActivity::class.java)
                startActivity(intent)
             }
+        viewModel.allMemos.observe(viewLifecycleOwner) { memos ->
+            // Update the cached copy of the words in the adapter.
+            memos.let { adapter.submitList(it) }
+        }
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.i("roomFragment12","error")
+
+        if (requestCode == newMypageFragmentRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(MongsAddActivity.EXTRA_REPLY)?.let { reply ->
+                val memo = Memo(reply)
+                viewModel.insert(memo)
+            }
+        } else {
+            Toast.makeText(
+                context,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
     override fun onDestroyView() {
         binding = null
         super.onDestroyView()
