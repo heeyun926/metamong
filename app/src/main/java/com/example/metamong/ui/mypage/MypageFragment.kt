64b7486a68c7e9603ec24bb768ca1a0a2e2649c2
heeyun926 <fkffkfk31@gmail.com>
2022-01-12 17:33:49 +0900

    chart data binding!

--- app/src/main/java/com/example/metamong/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/metamong/ui/mypage/MypageFragment.kt
@@ -1,6 +1,7 @@
 package com.example.metamong.ui.mypage
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,6 +14,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.metamong.R
 import com.example.metamong.databinding.FragmentMypageBinding
+import com.example.metamong.ui.mypage.history.HistoryActivity
 import com.example.metamong.ui.mypage.memo.MemoApplication
 import com.example.metamong.ui.mypage.memo.MemoListAdapter
 import com.example.metamong.ui.mypage.memo.MemoViewModel
@@ -68,6 +70,10 @@ class MypageFragment : Fragment() {
                     getString(R.string.app_name)
                 )
             this.findNavController().navigate(action)
+        }
+        binding!!.chartBg.setOnClickListener{
+                val intent = Intent(context, HistoryActivity::class.java)
+                startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/metamong/ui/mypage/history/HistoryActivity.kt
+++ app/src/main/java/com/example/metamong/ui/mypage/history/HistoryActivity.kt
@@ -2,24 +2,54 @@ package com.example.metamong.ui.mypage.history
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
-import com.example.metamong.R
 import com.example.metamong.databinding.ActivityHistoryBinding
-import com.github.mikephil.charting.data.ChartData
+import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+
+data class Score(
+    val score: Int
+)
 
 class HistoryActivity : AppCompatActivity() {
     private lateinit var binding : ActivityHistoryBinding
-    private val chartdata = ArrayList<ChartData>()
+    private lateinit var lineChart: LineChart
+    private var score = ArrayList<Score>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        lineChart = binding.linechart
+
+        setDataToLineChart()
     }
+
+    private fun setDataToLineChart(){
+        val entries: ArrayList<Entry> = ArrayList()
+
+        score = getScoreList()
+
+        for( i in score.indices){
+            val score = score[i]
+            entries.add(Entry(i.toFloat(),score.score.toFloat()))
+        }
+        val lineDataSet = LineDataSet(entries, "")
+
+        val data = LineData(lineDataSet)
+        lineChart.invalidate()
+    }
+    //simulate api call
+    //we are initialising it directly
+    private fun getScoreList(): ArrayList<Score>{
+        score.add(Score(0))
+        score.add(Score(51))
+        score.add(Score(100))
+
+        return score
+    }
+
 
 }
