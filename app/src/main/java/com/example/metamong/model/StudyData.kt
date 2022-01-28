38258e9e322815c910026a60f7bed50428ba72e6
heeyun <fkffkfk31@gmail.com>
2022-01-28 09:38:26 +0900

    ing

---
+++ app/src/main/java/com/example/metamong/model/StudyData.kt
@@ -0,0 +1,4 @@
+package com.example.metamong.model
+
+class StudyData {
+}
--- app/src/main/java/com/example/metamong/ui/cloud/StudyAdapter.kt
+++ app/src/main/java/com/example/metamong/ui/cloud/StudyAdapter.kt
@@ -3,20 +3,20 @@ package com.example.metamong.ui.cloud
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-//class StudyAdapter : RecyclerView.Adapter<StudyAdapter.Holder> {
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudyAdapter.Holder {
-//
-//    }
-//
-//    override fun onBindViewHolder(holder: StudyAdapter.Holder, position: Int) {
-//
-//    }
-//
-//    override fun getItemCount(): Int {
-//
-//    }
-//
-//    inner class holder():{
-//
-//    }
-//}
+class StudyAdapter : RecyclerView.Adapter<StudyAdapter.Holder> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudyAdapter.Holder {
+
+    }
+
+    override fun onBindViewHolder(holder: StudyAdapter.Holder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    inner class holder():{
+
+    }
+}
